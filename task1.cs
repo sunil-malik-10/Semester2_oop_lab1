869037f1818908e704abf1d67bdc38e5b1022cb0
sunil-malik-10 <maliksunil40100@gmail.com>
2025-01-29 13:31:22 +0500

    Add files via upload

---
+++ task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1
+{
+    internal class task1
+    {
+        static void World(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD!!");
+            Console.Write("HELLO WORLD!!");
+            Console.ReadKey();
+        }
+    }
+}
